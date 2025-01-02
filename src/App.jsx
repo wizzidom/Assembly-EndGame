7455dc3a1284d3e5783baaaef40ca4086f2e52a5
wizzidom <majolawisdom@gmail.com>
2025-01-02 23:29:42 +0200

    Adding header and languages section

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { languages } from "./languages";
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <main>
+      <header>
+        <h1>Assembly Endgame</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Guess the word within 8 attempts to keey the programming languages
+          safe from Assembly!
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      </header>
 
-export default App
+      <section className="game-status">
+        <h2>You Win</h2>
+        <p>Well done</p>
+      </section>
+
+      <section className="languages">
+        {languages.map((language) => (
+          <p
+            style={{
+              color: language.color,
+              background: language.backgroundColor,
+            }}
+          >
+            {language.name}
+          </p>
+        ))}
+      </section>
+    </main>
+  );
+}
